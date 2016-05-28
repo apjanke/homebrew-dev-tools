d74e762d6f0439e10e531c334a33247f10f3f52e
Andrew Janke <andrew@apjanke.net>
2016-05-28 03:52:29 -0400

    build-logs: support new log dir structure

--- cmd/brew-build-logs.rb
+++ cmd/brew-build-logs.rb
@@ -8,7 +8,7 @@
 fpp = which("fpp")
 abort "Please install fpp with `brew instal fpp`." unless fpp
 
-logd = ARGV.resolved_formulae.first.logs rescue HOMEBREW_LOGS/ARGV.named.first
+logd = ARGV.resolved_formulae.first.logs(:main) rescue HOMEBREW_LOGS/"Formula"/ARGV.named.first
 abort "#{logd} doesn't exist." unless logd.directory?
 
 exec "find #{logd} | #{fpp}"
